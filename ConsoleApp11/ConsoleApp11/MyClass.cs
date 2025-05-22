cfe13110dc2b3a740c3daf8bb1677816818df93a
ClyZeR1 <sergeistarodyb@gmail.com>
2025-05-22 11:12:51 +0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class MyClass
+    {
+        public string change;
+    }
+
+    public struct MyStruct
+    {
+        public string change;
+    }
+
+    public struct Notebook
+    {
+        public string Model;
+        public string Manufacturer;
+        public decimal Price;
+
+        public Notebook(string model, string manufacturer, decimal price)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Производитель: {Manufacturer}");
+            Console.WriteLine($"Цена: {Price} рублей");
+        }
+    }
---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass classInstance = new MyClass();
+            classInstance.change = "не изменено";
+
+            MyStruct structInstance = new MyStruct();
+            structInstance.change = "не изменено";
+
+            Notebook myNotebook = new Notebook("Aspire 7", "Acer", 55000.99m);
+
+            Console.WriteLine("До вызова методов:");
+            Console.WriteLine("MyClass: " + classInstance.change);
+            Console.WriteLine("MyStruct: " + structInstance.change);
+
+            ClassTaker(classInstance);
+            StruktTaker(structInstance);
+
+            Console.WriteLine("\nПосле вызова методов:");
+            Console.WriteLine("MyClass: " + classInstance.change);
+            Console.WriteLine("MyStruct: " + structInstance.change);
+
+            Console.WriteLine("\nИнформация о ноутбуке:");
+            myNotebook.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
